d53770dfb425cfac681095b0cb08da88b6e8546a
DavidGayer <davidgayer1@gmail.com>
2024-02-26 14:14:53 +0100

    Created PictureEntity entity

---
+++ src/main/java/com/davidgayer/poznavacka/model/PictureEntity.java
@@ -0,0 +1,34 @@
+package com.davidgayer.poznavacka.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class PictureEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "latin_name")
+    private String latinName;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "description")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", referencedColumnName = "id")
+    private Category Category;
+
+}
