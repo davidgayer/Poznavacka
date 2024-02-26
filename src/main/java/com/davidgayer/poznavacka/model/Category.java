4e563b3f8d52830215a1010116a4d51d9201fcba
DavidGayer <davidgayer1@gmail.com>
2024-02-26 14:14:21 +0100

    Created Category Entity

---
+++ src/main/java/com/davidgayer/poznavacka/model/Category.java
@@ -0,0 +1,28 @@
+package com.davidgayer.poznavacka.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "picture_entity")
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+
+    private PictureEntity pictureEntity;
+
+}
